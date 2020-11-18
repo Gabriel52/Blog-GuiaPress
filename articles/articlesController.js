3a0065ecb226687016ea7a20f0c8292e554c2ba0
Gabriel52 <gabrielbrune52@gmail.com>
2020-11-17 21:30:38 -0300

    'heroku'

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const categoryModel = require('../categories/categories');
+const categoryModel = require('../categories/Categories');
 const articleModel = require('./Article');
 const slugify = require('slugify');
 const adminAuth = require('../middleware/adminAuth');
